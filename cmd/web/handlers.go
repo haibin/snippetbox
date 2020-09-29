87bdea8b3b6914db007fb71b32afc15200a1a62e
Liu Haibin <liu.haibin@gmail.com>
2020-09-29 16:28:43 +0800

    Finish ch5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,87 +1,73 @@
 package main
 
 import (
-    "errors"
+	"errors"
 	"fmt"
-	// "html/template"
-    "net/http"
-    "strconv"
+	"net/http"
+	"strconv"
 
-    "github.com/haibin/snippetbox/pkg/models"
+	"github.com/haibin/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
+	if r.URL.Path != "/" {
 		app.notFound(w)
-        return
-    }
-    
-    s, err := app.snippets.Latest()
-    if err != nil {
-        app.serverError(w, err)
-        return
-    }
+		return
+	}
 
-    for _, snippet := range s {
-        fmt.Fprintf(w, "%v\n", snippet)
-    }
-	
-    // files := []string{
-    //     "./ui/html/home.page.tmpl",
-	// 	"./ui/html/base.layout.tmpl",
-	// 	"./ui/html/footer.partial.tmpl",
-    // }
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-    // ts, err := template.ParseFiles(files...)
-    // if err != nil {
-	// 	app.serverError(w, err)
-    //     return
-    // }
-
-    // err = ts.Execute(w, nil)
-    // if err != nil {
-	// 	app.serverError(w, err)
-    // }
+	// Use the new render helper.
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
 		app.notFound(w)
 		return
-    }
+	}
 
-    s, err := app.snippets.Get(id)
-    if err != nil {
-        if errors.Is(err, models.ErrNoRecord) {
-            app.notFound(w)
-        } else {
-            app.serverError(w, err)
-        }
-        return
-    }
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
-    fmt.Fprintf(w, "%v", s)
+	// Use the new render helper.
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
 
-    // Create some variables holding dummy data. We'll remove these later on
-    // during the build.
-    title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := "7"
+	// Create some variables holding dummy data. We'll remove these later on
+	// during the build.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := "7"
 
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, err)
-        return
-    }
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,70 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
-    "log"
+	"html/template"
+	"log"
 	"net/http"
-    "os"
-    "database/sql"
+	"os"
 
-    "github.com/haibin/snippetbox/pkg/models/mysql"
-    
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/haibin/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
-    errorLog *log.Logger
-    infoLog  *log.Logger
-    snippets *mysql.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *mysql.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
-    // Define a new command-line flag for the MySQL DSN string.
-    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command-line flag for the MySQL DSN string.
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    // To keep the main() function tidy I've put the code for creating a connection
-    // pool into the separate openDB() function below. We pass openDB() the DSN
-    // from the command-line flag.
-    db, err := openDB(*dsn)
-    if err != nil {
-        errorLog.Fatal(err)
-    }
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
-    // We also defer a call to db.Close(), so that the connection pool is closed
-    // before the main() function exits.
-    defer db.Close()
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 	app := &application{
-        errorLog: errorLog,
-        infoLog:  infoLog,
-        snippets: &mysql.SnippetModel{DB: db},
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &mysql.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
-	
+
 	srv := &http.Server{
-        Addr:     *addr,
-        ErrorLog: errorLog,
-        Handler:  app.routes(),
-    }
-	
-    infoLog.Printf("Starting server on %s", *addr)
-    err = srv.ListenAndServe()
-    errorLog.Fatal(err)
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 // The openDB() function wraps sql.Open() and returns a sql.DB connection pool
 // for a given DSN.
 func openDB(dsn string) (*sql.DB, error) {
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        return nil, err
-    }
-    if err = db.Ping(); err != nil {
-        return nil, err
-    }
-    return db, nil
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
